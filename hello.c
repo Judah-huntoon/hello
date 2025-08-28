112a486d2e959c37f0602078e40cac0ee2b7b867
Judah-huntoon <jhuntoon3@huskers.unl.edu>
2025-08-28 16:36:03 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Judah Huntoon Trey Siske
+ * Date: 2025/08/28
+ *
+ * A simple name program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Judah Huntoon and Trey Siske\n");
+
+  return 0;
+}
